5a0ad1f7f45f7f321a53cb0080306ab25fb5ade1
Sérgio Carvalho <sergio.carvalho@sooma.com>
2019-07-02 15:15:01 +0100

    html.element: Initial commit

---
+++ templates/text-html/_/sergiosgc/html.element/all.php
@@ -0,0 +1,36 @@
+<?php
+if (!isset($tvars['html.element'])) throw new \Exception('Component template requires $tvars[\'html.element\']');
+(function($element) {
+    if (is_string($element)) $element = json_decode($element, true, 512, JSON_THROW_ON_ERROR | JSON_BIGINT_AS_STRING);
+    $stack = [ [ $element, false ] ];
+    while (count($stack)) {
+        list($node, $opened) = array_pop($stack);
+        if ($opened) {
+            printf('</%s>', $node['element']);
+            continue;
+        } 
+        if (isset($node['element'])) {
+            array_push($stack, [ $node, true ]);
+            printf('<%s>',
+                implode(
+                    ' ',
+                    array_filter(array_merge(
+                        [ $node['element'] ],
+                        array_map(
+                            function($argName, $argValue) {
+                                if ($argName[0] != '@') return '';
+                                return sprintf('%s="%s"', htmlspecialchars(substr($argName, 1)), htmlspecialchars($argValue));
+                            },
+                            array_keys($node),
+                            $node
+                        )
+                    ))
+            ));
+            if ($node['children']) for($i=count($node['children'])-1; $i>=0; $i--) array_push($stack, [ $node['children'][$i], false ]);
+        } elseif (isset($node['text'])) {
+            print(htmlspecialchars($node['text']));
+        } elseif (isset($node['raw'])) {
+            print($node['raw']);
+        }
+    }
+})($tvars['html.element']);
