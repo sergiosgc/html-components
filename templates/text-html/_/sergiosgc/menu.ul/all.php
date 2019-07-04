3811c345e7688b4dede36df244f8de53cbd2dc90
Sérgio Carvalho <sergio.carvalho@sooma.com>
2019-07-04 17:06:21 +0100

    menu.ul: Initial commit

---
+++ templates/text-html/_/sergiosgc/menu.ul/all.php
@@ -0,0 +1,18 @@
+<ul class="<?= isset($tvars['menu']['class']) ? $tvars['menu']['class'] : 'menu' ?>"<?= isset($tvars['menu']['id']) ? ' id="' . $tvars['menu']['id'] . '"' : '' ?>>
+<?php 
+foreach ($tvars['menu']['items'] as $item) { 
+    if ($item['active']) $item['class'] = isset($item['class']) ? $item['class'] . ' active' : 'active';
+    if ($item['submenu']) $item['class'] = isset($item['class']) ? $item['class'] . ' hassubmenu' : 'hassubmenu';
+    printf('<li class="%s"><a href="%s">%s</a>%s</li>',
+        array_reduce(
+            explode('/', $item['href']),
+            function ($acc, $part) { if ($part == "") return $acc; return $acc . ' ' . array_reverse(explode(' ', $acc))[0] . '-' . $part; },
+            (isset($item['class']) ? $item['class'] . ' ': '') . 'root'
+        ),
+        $item['href'], 
+        $item['label'],
+        isset($item['submenu']) ? \sergiosgc\output\Negotiated::$singleton->stemplate('/_/sergiosgc/menu.ul/', [ 'menu' => [ 'items' => $item['submenu'] ]]) : ''
+    );
+}
+?>
+</ul>
