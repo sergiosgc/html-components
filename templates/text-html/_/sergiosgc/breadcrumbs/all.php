1abce03459c8f755f707970bab2d85279b60aae0
Sérgio Carvalho <sergio.carvalho@sooma.com>
2019-12-18 16:30:34 +0000

    breadcrumbs: Do not output empty spans, so result passes tidy HTML validator

--- templates/text-html/_/sergiosgc/breadcrumbs/all.php
+++ templates/text-html/_/sergiosgc/breadcrumbs/all.php
@@ -1,8 +1,8 @@
 <?php
 $breadcrumbs = $tvars['breadcrumbs'];
 
-printf('<span class="breadcrumbs">%s</span>', 
-    implode('<span class="breadcrumb-separator"></span>', array_map(
+if ($breadcrumbs && count($breadcrumbs)) printf('<span class="breadcrumbs">%s</span>', 
+    implode('<span class="breadcrumb-separator">&#8203;</span>', array_map(
         function($label, $url) {
             return sprintf('<a href="%s" class="breadcrumb">%s</a>', htmlspecialchars($url), $label);
         },
